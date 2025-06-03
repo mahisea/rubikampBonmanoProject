4229bd70e3c0a10cb49e0f54340771b760088828
Mahi <mimarbabi80@gmail.com>
2025-06-03 21:40:48 +0430

    change HomePage UL and add a button

--- src/pages/client/Home/HomePage.jsx
+++ src/pages/client/Home/HomePage.jsx
@@ -1,20 +1,45 @@
 import { ThemeContext } from "@/context/ThemeContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    const {theme, setTheme} = useContext(ThemeContext);
-    
-    return (
-        <div style={{
-            backgroundColor: theme === 'light' ? 'white' : 'black',
-             color: theme === 'light' ? 'black' : 'white' 
-            }}>
-            <h1>HomePage</h1>
-            <button onClick={() => {
-                setTheme(theme === 'light' ? 'dark' : 'light');
-            }}>Change Theme: {theme}</button>
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  //   const = navigate
+
+  return (
+    <div
+    >
+      <div
+        style={{
+          backgroundImage: "url('/11.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          color: "white",
+          filter: theme === "blur" ? "blur(5px)" : "blur(0)",
+        }}
+      >
+        <div style={{zIndex: "10"}}>
+          <h1 style={{ textAlign: "center", fontSize:"80px" }}>Mahi</h1>
+          <button
+            onClick={() => {
+              setTheme(theme === "blur" ? "none" : "blur");
+            }}
+          >
+            Change Flter: {theme}
+          </button>
+          <Link to="/auth">
+            <button>login / Sign Up</button>
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
